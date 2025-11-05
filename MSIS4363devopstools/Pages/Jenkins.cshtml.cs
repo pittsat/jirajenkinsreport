8324243535abed02b39a80589daaea315b41e297
Pitts, Aaron <pittsa@okstate.edu>
2025-11-05 13:25:10 -0600

    I kind of went crazy

---
+++ MSIS4363devopstools/Pages/Jenkins.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSIS4363devopstools.Pages
+{
+    public class JenkinsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
